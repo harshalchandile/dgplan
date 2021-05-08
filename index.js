972b4b7a4c4089c69149573e349c5cf52c5fb76f
harshal chandile <harshal.chandile767@gmail.com>
2021-05-08 10:32:46 +0000

    initial master

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://db_user:1b0oqzRrbbZrziDC@dailyget.9id5p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://db_user:1b0oqzRrbbZrziDC@dailyget.9id5p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 const users = require('../controllers/user-api');
 const transactions = require('../controllers/transaction-api');
 
 let numb = 0;
-while(1){
-numb+=1;
-users.getUsers(numb);
-transactions.getTransactions(numb);
+while (1) {
+  numb += 1;
+  users.getUsers(numb);
+  transactions.getTransactions(numb);
+}
